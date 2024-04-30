6f33f3b8fdea9b83fb03a96c4dc3d5cc428efd0f
ilik100 <ilya.ilik100@gmail.com>
2024-04-30 22:53:36 +0300

    correcting

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Ongoyings from "./components/Ongoyings/Ongoyings";
+import Search_filter_in_react_project from "./components/Search_filter_in_react_project/Search_filter_in_react_project";
 
 function App(props) {
   return (
     <div className="App">
-      <Header headerData={props.state.headerData}/>
+      {/* <Header headerData={props.state.headerData}/> */}
       {/* <Ongoyings ongoyingsData={props.state.ongoyingsData}/> */}
+      <Search_filter_in_react_project />
     </div>
   );
 }
--- src/components/Search_filter_in_react_project/Search_filter_in_react_project.jsx
+++ src/components/Search_filter_in_react_project/Search_filter_in_react_project.jsx
@@ -1,12 +1,32 @@
-import React from "react";
-import "./Header.css";
+import React, { useState } from "react";
+// import "./Header.css";
 
 const Search_filter_in_react_project = () => {
+  // const filterTest = (searchText, listOfTest) => { // принемаем аргументами поисковое слово и массив данных
+  //   if (!searchText) { // если поисковое слово пустое то  
+  //     return listOfTest; // возвращаем массив бе изменнений
+  //   }
+  //   else {
+  //     return listOfTest.filter();
+  //   }
+  // };
 
+  const fruits = ["яблоко", "банан", "апельсин"];
+
+  const [searchTerm, setSearchTerm] = useState(""); // это пустая строка для начального  стейта
 
   return (
     <div>
-
+      <div>Живой поиск</div>
+      <div>
+        <input
+          value={searchTerm}
+          type="text"
+          placeholder="Поиск"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          autoComplete="off"
+        />
+      </div>
     </div>
   );
 };
