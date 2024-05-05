ac45d41ca5b29928e154087349143db6683d21b2
ilik100 <ilya.ilik100@gmail.com>
2024-05-05 03:31:04 +0300

    wrote a script to change the background at the press of a button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
-import "./App.css";
-import Header from "./components/Header/Header";
-import Ongoyings from "./components/Ongoyings/Ongoyings";
-import Search from "./Search";
+import React from "react"; // импортируем реакт
+import "./App.css"; // импортируем стили 
+import Header from "./components/Header/Header"; // импортируем компоненту Header
+import Ongoyings from "./components/Ongoyings/Ongoyings"; // импортируем компоненту Ongoyings 
+import Search from "./Search"; // импортируем компоненту Search 
 
 function App(props) {
   return (
     <div>
-      <Header />
-      <Ongoyings ongoyingsData={props.state.ongoyingsData}/>
-      <Search />
+      <Header /> {/* компонента Header */}
+      {/* <Ongoyings ongoyingsData={props.state.ongoyingsData}/> компонента Ongoyings с переданными в нее пропсами */}
+      {/* <Search /> компонента Search */}
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,30 @@
-import React from "react";
-import "./Header.css";
+import React from "react"; // импортируем реакт
+import "./Header.css"; // импортируем стили
 
 const Header = () => {
   const iconSearch =
-    "https://icon-icons.com/icons2/2406/PNG/512/search_magnifier_icon_145939.png";
+    "https://icon-icons.com/icons2/2406/PNG/512/search_magnifier_icon_145939.png"; // константа содержащяя в себе картинку
+
+  let refdiv1 = React.createRef();
+
+  // Функция для смены фона Header по нажатию на кнопку тема
+  function handleClick(event) {                      // создаёт функцию handleClick, которая принимает аргумент event
+    const target = event.target;                     // создаёт константу target, равную значению свойства event.target. Это позволяет получить доступ к элементу, на котором произошло событие
+    if (target.className.indexOf("active") === -1) { // проверяет, содержит ли класс элемента значение «active». Если результат равен -1, то условие истинно и выполняется следующий блок
+      target.classList.add("active");                // добавляет класс «active» к текущему элементу
+      const refdiv1v1 = refdiv1.current;             // создает константу к которой обращаться по рефу
+      refdiv1v1.style.background = "#FFFFFF";        // сменить фон блока где весит реф
+    } else {                                         // если условие в начале блока ложно, то выполняется этот блок
+      target.classList.remove("active");             // удаляет класс «active» из текущего элемента
+      const refdiv1v1 = refdiv1.current;             // создает константу к которой обращаться по рефу
+      refdiv1v1.style.background = "#181d2d";        // сменить фон блока где весит реф
+    }
+  }
+
+
 
   return (
-    <div>
+    <div className="header-container" ref={refdiv1}>
       <div className="header">
         <div className="header-btn-menu"></div>
         <div className="header-logo">
@@ -14,10 +32,16 @@ const Header = () => {
           <p className="logo-text-right">VIE</p>
         </div>
         <div className="search-anim">
-          <input className="input-search-anim" placeholder="Поиск по сайту..." type="text"  />
+          <input
+            className="input-search-anim"
+            placeholder="Поиск по сайту..."
+            type="text"
+          />
           <img className="btn-search-anim" src={iconSearch} alt="Error" />
         </div>
-        <div className="theme-toggle">Тема</div>
+        <div className="theme-toggle" onClick={handleClick}>
+          Тема
+        </div>
         <div className="header-btn-login">Логин</div>
       </div>
     </div>
